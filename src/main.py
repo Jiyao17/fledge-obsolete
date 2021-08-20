0e01d5871d55d2d7ff3e77202c9a2e93db6460cc
Jiyao17 <jiyao.liu@outlook.com>
2021-08-20 01:52:29 -0400

    argparser tested

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 
-import sys
 from typing import List
 from argparse import ArgumentParser
 
@@ -12,25 +11,19 @@ from utils.client import Client
 def get_argument_parser() -> ArgumentParser:
     ap = ArgumentParser()
     # positional
-    ap.add_argument("task")
+    ap.add_argument("task", type=str)
     ap.add_argument("g_epoch_num", type=int)
     ap.add_argument("client_num", type=int)
     ap.add_argument("l_data_num", type=int)
     ap.add_argument("l_epoch_num", type=int)
     ap.add_argument("l_batch_size", type=int)
     ap.add_argument("l_lr", type=float)
-    #optional
-    ap.add_argument("-p", "--datapath", default="~/projects/fledge/data/")
-    ap.add_argument("-d", "--device", default="cpu")
+    # optional
+    ap.add_argument("-p", "--datapath", type=str, default="~/projects/fledge/data/")
+    ap.add_argument("-d", "--device", type=str, default="cpu")
+    ap.add_argument("-r", "--result_file", type=str, default="./result.txt")
 
     return ap
-
-# def run_FashionMNIST(server: Server, clients: List[Client]):
-#     pass
-
-# def run_SpeechCommand(server: Server, clients: List[Client]):
-#     pass
-
 
 def run_sim(server: Server, clients: List[Client]):
 
@@ -67,6 +60,7 @@ if __name__ == "__main__":
     # shared settings
     DATA_PATH: str = args.datapath
     DEVICE: str = torch.device(args.device)
+    RESULT_FILE: str = args.result_file
 
     clients: List[Client] = [
         Client(TASK, L_EPOCH_NUM, DEVICE) 
@@ -74,8 +68,8 @@ if __name__ == "__main__":
         ]
     server = Server(TASK, CLIENT_NUM, DEVICE, G_EPOCH_NUM)
 
-    print("Args: %s %d %d %d %d %d %f %s %s" %
-        (TASK, G_EPOCH_NUM, CLIENT_NUM, L_DATA_NUM, L_EPOCH_NUM, L_BATCH_SIZE, L_LR, DATA_PATH, DEVICE)
+    print("Args: %s %d %d %d %d %d %f %s %s %s" %
+        (TASK, G_EPOCH_NUM, CLIENT_NUM, L_DATA_NUM, L_EPOCH_NUM, L_BATCH_SIZE, L_LR, DATA_PATH, DEVICE, RESULT_FILE)
         )
 
     # run_sim(server, clients)
@@ -88,3 +82,9 @@ if __name__ == "__main__":
     #     run_SpeechCommand(server, clients)
     # else:
     #     raise "Unsupported task."
+
+# def run_FashionMNIST(server: Server, clients: List[Client]):
+#     pass
+
+# def run_SpeechCommand(server: Server, clients: List[Client]):
+#     pass
