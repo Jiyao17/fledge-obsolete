820f15304d017904cc1febb2d8b97bf0ec711663
Jiyao17 <jiyao.liu@outlook.com>
2021-09-24 20:59:40 -0400

    new client tested

--- src/test.py
+++ src/test.py
@@ -1,38 +1,167 @@
 
-from typing import List
+import torch
+from torchtext.datasets import AG_NEWS
+from torch.utils.data import DataLoader
+from torch import nn
+import time
 
-from utils.client import Client
-from utils.funcs import get_partitioned_datasets, get_test_dataset
+class TextClassificationModel(nn.Module):
+
+    def __init__(self, vocab_size, embed_dim, num_class):
+        super(TextClassificationModel, self).__init__()
+        self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=True)
+        self.fc = nn.Linear(embed_dim, num_class)
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.5
+        self.embedding.weight.data.uniform_(-initrange, initrange)
+        self.fc.weight.data.uniform_(-initrange, initrange)
+        self.fc.bias.data.zero_()
+
+    def forward(self, text, offsets):
+        embedded = self.embedding(text, offsets)
+        return self.fc(embedded)
 
 
-task = "AG_NEWS"
-client_num = 1
-l_data_num = 10000
-l_batch_size = 64
-l_epoch_num = 5
-l_lr = 0.01
-device = "cuda"
-data_path = "/home/tuo28237/projects/fledge/data/"
 
-datasets = get_partitioned_datasets(task, client_num, l_data_num, l_batch_size, data_path)
-test_dataset = get_test_dataset(task, data_path)
-# initialize server and clients
-# train_iter = AG_NEWS(split="train")
-# train_dataset = to_map_style_dataset(train_iter)
-clients: List[Client] = [
-    Client(task, datasets[i], l_epoch_num, l_batch_size, l_lr, device) 
-    for i in range(client_num)
-    ]
+class ClientAGNEWS:
+    def __init__(self) -> None:
+        from torchtext.data.utils import get_tokenizer
+        from torchtext.vocab import build_vocab_from_iterator
 
-for i in range(100):
-    print("Epoch %d ......" % i)
+        self.tokenizer = get_tokenizer('basic_english')
+        self.train_iter = AG_NEWS(split='train')
+        self.vocab = build_vocab_from_iterator(self.yield_tokens(self.train_iter), specials=["<unk>"])
+        self.vocab.set_default_index(self.vocab["<unk>"])
+        
+        self.text_pipeline = lambda x: self.vocab(self.tokenizer(x))
+        self.label_pipeline = lambda x: int(x) - 1
 
-    # server.distribute_model()
-    for j in range(len(clients)):
-        # acc = clients[j].test_model()
-        # print("before training: client %d accuracy %.9f at epoch %d" % (j, acc, i))
-        acc = clients[j].train_model()
-        # print("training acc:  client %d accuracy %.9f at epoch %d" % (j, acc, i))
-        acc = clients[j].test_model()
-        print("after training:  client %d accuracy %.9f at epoch %d" % (j, acc, i))
 
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        train_iter = AG_NEWS(split='train')
+        self.dataloader = DataLoader(train_iter, batch_size=8, shuffle=False, collate_fn=self.collate_batch)
+
+        train_iter = AG_NEWS(split='train')
+        self.num_class = len(set([label for (label, text) in train_iter]))
+        self.vocab_size = len(self.vocab)
+        self.emsize = 64
+        self.model = TextClassificationModel(self.vocab_size, self.emsize, self.num_class).to(self.device)
+
+
+        self.criterion = None
+        self.optimizer = None
+        self.scheduler = None
+
+
+    def yield_tokens(self, data_iter):
+        for _, text in data_iter:
+            yield self.tokenizer(text)
+
+    def collate_batch(self, batch):
+        label_list, text_list, offsets = [], [], [0]
+        for (_label, _text) in batch:
+            label_list.append(self.label_pipeline(_label))
+            processed_text = torch.tensor(self.text_pipeline(_text), dtype=torch.int64)
+            text_list.append(processed_text)
+            offsets.append(processed_text.size(0))
+        label_list = torch.tensor(label_list, dtype=torch.int64)
+        offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
+        text_list = torch.cat(text_list)
+        return label_list.to(self.device), text_list.to(self.device), offsets.to(self.device)
+
+    def train(self, dataloader, epoch):
+        self.model.train()
+        total_acc, total_count = 0, 0
+        log_interval = 500
+        start_time = time.time()
+
+        for idx, (label, text, offsets) in enumerate(dataloader):
+            self.optimizer.zero_grad()
+            predicted_label = self.model(text, offsets)
+            loss = self.criterion(predicted_label, label)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.1)
+            self.optimizer.step()
+
+
+            total_acc += (predicted_label.argmax(1) == label).sum().item()
+            total_count += label.size(0)
+            if idx % log_interval == 0 and idx > 0:
+                elapsed = time.time() - start_time
+                print('| epoch {:3d} | {:5d}/{:5d} batches '
+                    '| accuracy {:8.3f}'.format(epoch, idx, len(dataloader),
+                                                total_acc/total_count))
+                total_acc, total_count = 0, 0
+                start_time = time.time()
+
+    def evaluate(self, dataloader):
+        self.model.eval()
+        total_acc, total_count = 0, 0
+
+        with torch.no_grad():
+            for idx, (label, text, offsets) in enumerate(dataloader):
+                predicted_label = self.model(text, offsets)
+                loss = criterion(predicted_label, label)
+                total_acc += (predicted_label.argmax(1) == label).sum().item()
+                total_count += label.size(0)
+        return total_acc/total_count
+
+    def run(self):
+        import time
+
+        from torch.utils.data.dataset import random_split
+        from torchtext.data.functional import to_map_style_dataset
+        # Hyperparameters
+        EPOCHS = 10 # epoch
+        LR = 5  # learning rate
+        BATCH_SIZE = 64 # batch size for training
+
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=LR)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, 1.0, gamma=0.1)
+        total_accu = None
+        train_iter, test_iter = AG_NEWS()
+        train_dataset = to_map_style_dataset(train_iter)
+        test_dataset = to_map_style_dataset(test_iter)
+        num_train = int(len(train_dataset) * 0.95)
+        split_train_, split_valid_ = \
+            random_split(train_dataset, [num_train, len(train_dataset) - num_train])
+
+        train_dataloader = DataLoader(split_train_, batch_size=BATCH_SIZE,
+                                    shuffle=True, collate_fn=self.collate_batch)
+        valid_dataloader = DataLoader(split_valid_, batch_size=BATCH_SIZE,
+                                    shuffle=True, collate_fn=self.collate_batch)
+        test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE,
+                                    shuffle=True, collate_fn=self.collate_batch)
+
+        for epoch in range(1, EPOCHS + 1):
+            epoch_start_time = time.time()
+            self.train(train_dataloader, epoch)
+            # accu_val = evaluate(valid_dataloader)
+            # if total_accu is not None and total_accu > accu_val:
+            #   scheduler.step()
+            # else:
+            #    total_accu = accu_val
+            print('-' * 59)
+            # print('| end of epoch {:3d} | time: {:5.2f}s | '
+            #     'valid accuracy {:8.3f} '.format(epoch,
+            #                                     time.time() - epoch_start_time,
+            #                                     accu_val))
+            print('| end of epoch {:3d} | time: {:5.2f}s | '.format(epoch,
+                                                time.time() - epoch_start_time,
+                                                ))
+            print('-' * 59)
+
+        print('Checking the results of test dataset.')
+        accu_test = self.evaluate(test_dataloader)
+        print('test accuracy {:8.3f}'.format(accu_test))
+
+
+
+
+client = ClientAGNEWS()
+
+client.run()
